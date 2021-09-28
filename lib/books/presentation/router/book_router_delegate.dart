93dc7da87908730557f3f174fdb09a78fabff56d
comrade77 <nimec77@gmail.com>
2021-09-28 16:55:16 +0700

    Надо пробовать переделать под bloc

--- lib/books/presentation/router/book_router_delegate.dart
+++ lib/books/presentation/router/book_router_delegate.dart
@@ -4,7 +4,6 @@ import 'package:flutter_imdb/books/domain/entities/book.dart';
 import 'package:flutter_imdb/books/domain/ports/repositories/book_repository.dart';
 import 'package:flutter_imdb/books/presentation/pages/book_details_page.dart';
 import 'package:flutter_imdb/books/presentation/router/book_route_path.dart';
-import 'package:flutter_imdb/books/presentation/screens/book_details_screen.dart';
 import 'package:flutter_imdb/books/presentation/screens/books_list_screen.dart';
 import 'package:flutter_imdb/books/presentation/screens/unknown_screen.dart';
 
